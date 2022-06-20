cf7788c2ccb4ac974b5304cb3357444c871e7202
jupidev1586 <gianpaolobuono@gmail.com>
2022-06-20 15:42:45 +0200

    chore: esercizi 20-06-2022

--- JAVASCRIPT_06_20_06_2022/script.js
+++ JAVASCRIPT_06_20_06_2022/script.js
@@ -12,57 +12,60 @@
 // operation( sum, 1 , 3 ) -> 4
 // operation( div, 1 , 0 ) -> Errore: non divisibile per zero :(
 
+
+
+
 // 1 -> Sciviamo una funzione che accetta due parametri e torna la somma di entrambi
 // sum(1,3); -> 4
-// function sum(a, b) {
-//   console.log( a + b );
-//   return a + b;
-// }
+function sum(a, b) {
+  console.log( a + b );
+  return a + b;
+}
 
 
-// // 2 -> Scriviamo una versione miglirata della prima che controlla l'esistenza dei due parametri e che siano numeri
-// // sum(1) -> Errore in console che spiega cosa non va
-// function sum(a, b) {
-//   if (typeof a === 'number' || typeof b === 'number') {
-//     console.log( a + b );
-//     return a + b;
-//   } else {
-//     console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
-//   }
-// }
+// 2 -> Scriviamo una versione miglirata della prima che controlla l'esistenza dei due parametri e che siano numeri
+// sum(1) -> Errore in console che spiega cosa non va
+function sum(a, b) {
+  if (typeof a === 'number' || typeof b === 'number') {
+    console.log( a + b );
+    return a + b;
+  } else {
+    console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
+  }
+}
 
-// // 3 -> Scriviamo come per il punto due, anche per moltiplicazione, divisione, e sottrazione
-// // div(2,0) -> il programma di deve dire che non posso dividere per 0
-// function sub(a, b) {
-//   if (typeof a === 'number' || typeof b === 'number') {
-//     console.log( a - b );
-//     return a - b;
-//   } else {
-//     console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
-//   }
-// }
+// 3 -> Scriviamo come per il punto due, anche per moltiplicazione, divisione, e sottrazione
+// div(2,0) -> il programma di deve dire che non posso dividere per 0
+function sub(a, b) {
+  if (typeof a === 'number' || typeof b === 'number') {
+    console.log( a - b );
+    return a - b;
+  } else {
+    console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
+  }
+}
 
-// function mul(a, b) {
-//   if (typeof a === 'number' || typeof b === 'number') {
-//     console.log( a * b );
-//     return a * b;
-//   } else {
-//     console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
-//   }
-// }
+function mul(a, b) {
+  if (typeof a === 'number' || typeof b === 'number') {
+    console.log( a * b );
+    return a * b;
+  } else {
+    console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
+  }
+}
 
-// function div(a, b) {
-//   if ((typeof a === 'number' && a != 0) || (typeof b === 'number' && b != 0)) {
-//     if ( a != 0 && b != 0 ) {
-//       console.log( a / b );
-//       return a / b;
-//     } else {
-//       console.error( 'I numeri inseriti non sono divisibili tra loro, perchè uno dei 2 è uguale a 0' );
-//     }
-//   } else {
-//     console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
-//   }
-// }
+function div(a, b) {
+  if ((typeof a === 'number' && a != 0) || (typeof b === 'number' && b != 0)) {
+    if ( a != 0 && b != 0 ) {
+      console.log( a / b );
+      return a / b;
+    } else {
+      console.error( 'I numeri inseriti non sono divisibili tra loro, perchè uno dei 2 è uguale a 0' );
+    }
+  } else {
+    console.error( 'Non hai inserito degli argomenti validi. Non sono numeri.' );
+  }
+}
 
 
 // document.addEventListener('keydown', function(event) {
@@ -100,11 +103,16 @@
 // });
 
 
+
+
+
+
+
+
 // 4 -> Scriviamo una funzione che possa eseguire le 4 precedenti
 // es:
 // operation( sum, 1 , 3 ) -> 4
 // operation( div, 1 , 0 ) -> Errore: non divisibile per zero :(
-
 function calculate(operation, a, b) {
 
   if (typeof a === 'number' && typeof b === 'number') {
