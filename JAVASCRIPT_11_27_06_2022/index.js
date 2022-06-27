26605df6e79d507b645283ea7481594b6f65570a
jupidev1586 <gianpaolobuono@gmail.com>
2022-06-27 14:56:37 +0200

    chore: esercizio 27-06.2022

---
+++ JAVASCRIPT_11_27_06_2022/index.js
@@ -0,0 +1,43 @@
+const userPrompt = prompt('Inserisci il tuo username qui...');
+
+const createEl = (el) => document.createElement(el);
+
+const loginSection = () => {
+  const headerEl = createEl('header');
+  const containerEl = createEl('div');
+  const navEl = createEl('nav');
+  const pEl = createEl('p');
+  
+  const body = document.body;
+  
+  headerEl.className = 'header'; 
+  containerEl.className = 'container'; 
+  navEl.className = 'navbar'; 
+  pEl.className = 'paragraph'; 
+  
+  body.append(containerEl);
+  containerEl.appendChild(headerEl);
+  headerEl.appendChild(navEl);
+  navEl.appendChild(pEl);
+  localStorage.setItem('username', 'jupi');
+  pEl.textContent = 'Benvenuto, ' + (localStorage.getItem('username').toUpperCase());
+}
+
+const spinnerEl = document.querySelector('.cont-spinner');
+
+try {
+  if(userPrompt === 'jupi') {
+    loginSection();
+  } else {
+    throw new Error(
+      `
+        ${alert('Username errato')}
+      `
+    )
+  }
+} catch (error) {
+  console.error(error);
+  spinnerEl.style.display = 'block';
+} finally {
+  console.log('Default');
+}
